649c812fe4028db43d0f407b96978689dbcd7b9a
Abhishek Aute <abhishekaute2002@gmail.com>
2022-08-05 17:01:17 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -11,6 +11,7 @@ def equalpress():
 	try:
 
 		global expression
+		
 		total = str(eval(expression))
 		equation.set(total)
 		expression = ""
@@ -36,10 +37,10 @@ if __name__ == "__main__":
 	Display  = Entry(gui,bd=30,bg="SteelBlue3", textvariable=equation,font =('ariel',20,"bold"),justify ="right")
 	Display.grid(columnspan=5, row=0)
 
-	
+
 	AC = Button(gui,font=("bold") ,text='AC',bg='pink',command=clear, height=1, width=6,bd=11)
 	AC.grid(row=2, column=3)
-	
+
 	button1 = Button(gui,font=("bold") , text=' 1 ',bg='light skyblue',command=lambda: press(1), height=1, width=6,bd=11)
 	button1.grid(row=3, column=0)
 	button2 = Button(gui,font=("bold") , text=' 2 ',bg='light skyblue',command=lambda: press(2), height=1, width=6,bd=11)
@@ -48,7 +49,7 @@ if __name__ == "__main__":
 	button3.grid(row=3, column=2)
 	plus    = Button(gui,font=("bold") , text='+',bg='light skyblue',command=lambda: press("+"), height=1, width=6,bd=11)
 	plus.grid(row=6, column=2)
-	
+
 	button4  = Button(gui,font=("bold") ,text=' 4 ',bg='light skyblue',command=lambda: press(4), height=1, width=6,bd=11)
 	button4.grid(row=4, column=0)
 	button5  = Button(gui,font=("bold") ,text=' 5 ',bg='light skyblue',command=lambda: press(5), height=1, width=6,bd=11)
@@ -57,7 +58,7 @@ if __name__ == "__main__":
 	button6.grid(row=4, column=2)
 	minus    = Button(gui,font=("bold") ,text=' - ',bg='light skyblue',command=lambda: press("-"), height=1, width=6,bd=11)
 	minus.grid(row=4, column=3)
-	
+
 	button7  = Button(gui,font=("bold") , text=' 7 ',bg='light skyblue',command=lambda: press(7), height=1, width=6,bd=11)
 	button7.grid(row=5, column=0)
 	button8  = Button(gui,font=("bold") , text=' 8 ',bg='light skyblue',command=lambda: press(8), height=1, width=6,bd=11)
@@ -67,7 +68,7 @@ if __name__ == "__main__":
 	multiply = Button(gui,font=("bold") , text=' x ',bg='light skyblue',command=lambda: press("*"), height=1, width=6,bd=11)
 	multiply.grid(row=5, column=3)
 
-	
+
 	Decimal = Button(gui,font=("bold") , text='.', bg='light skyblue',command=lambda: press('.'), height=1, width=6,bd=11)
 	Decimal.grid(row=6, column=0)
 	button0 = Button(gui,font=("bold") , text=' 0 ', bg='light skyblue',command=lambda: press(0), height=1, width=6,bd=11)
@@ -76,6 +77,6 @@ if __name__ == "__main__":
 	equal.grid(row=3, column=3)
 	divide  = Button(gui,font=("bold") , text=' ÷ ', bg='light skyblue',command=lambda: press("/"),height=1,width=6,bd=11)
 	divide.grid(row=6, column=3)
-	
-	
+
+
 	gui.mainloop()
